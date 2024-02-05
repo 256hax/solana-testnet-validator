7f76436a2e816c85b12b8a7aa6f94f4d1142a773
256hax <256hax@gmail.com>
2024-02-05 09:13:45 +0900

    ref: remove unused address

--- shell/airdrop.sh
+++ shell/airdrop.sh
@@ -6,11 +6,9 @@ solana config set --url $network
 
 # Set Accounts for airdrop
 # 8XR... Validator Account
-# 6Px... Vote Account
 # CJs... Backup Account
 accounts='
 8XRkWmiF672BvqLvkNnt8ggaywyTQxoH3pyWAWtEGzzp
-6Pxb3bmmTPxmza8mkczTMhoLny4rqLX6A94yPwtEpozL
 CJsPSQtV28CJiRt8XThuG5Ei1cX2fH5GcPoZYyM26gzm
 '
 
